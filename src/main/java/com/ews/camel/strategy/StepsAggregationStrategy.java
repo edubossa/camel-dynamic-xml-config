9d79b96a93072dfd7461b5ec1d4688635e6f1319
Eduardo Wallace da Silva <eduardo.wallace@hdi.com.br>
2020-03-11 11:08:34 -0300

    rename component

--- src/main/java/com/ews/camel/strategy/StepsAggregationStrategy.java
+++ src/main/java/com/ews/camel/strategy/StepsAggregationStrategy.java
@@ -11,7 +11,7 @@ import java.util.Date;
 import java.util.List;
 
 @Component
-public class HttpMultiCastAggregationStrategy implements AggregationStrategy {
+public class StepsAggregationStrategy implements AggregationStrategy {
 
     @lombok.SneakyThrows
     public Exchange aggregate(Exchange oldExchange, Exchange newExchange) {
